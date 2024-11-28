81b531184d4a1dccc91dffb210b54a1460a8d45b
Gustaxz <guxtavogusteible@gmail.com>
2024-11-28 19:54:16 -0300

    FRONT

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,76 +1,186 @@
-
-import { Link, useNavigate } from "react-router-dom";
-import "./style.css";
-import { AuthService } from "../../store/auth/auth.service";
-import { useState } from "react";
-
+import { useAtom } from "jotai"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import { authAtom } from "../../store/auth/auth.module"
+import { AuthService } from "../../store/auth/auth.service"
+import "./style.css"
 
 export const Login = () => {
-  const [credentials, setCredentials] = useState({
-    email: "joao@gabriel.com",
-    password: "hfkg",
-  });
+	const [screen, setScreen] = useState<"login" | "faceid">("login")
+	const [faceImg, setFaceImg] = useState<File | null>(null)
+	const [auth] = useAtom(authAtom)
 
-  const navigate = useNavigate();
+	const [credentials, setCredentials] = useState({
+		email: "joao@gabriel.com",
+		password: "hfkg",
+	})
 
-  const { signIn } = AuthService();
+	const navigate = useNavigate()
 
-  const validateForm = async () => {
-    if (await signIn(credentials.email, credentials.password)) {
-      return navigate("/validate-code");
-    }
-  }
+	const { signIn, faceId } = AuthService()
 
-  return (
-    <main className="container">
-      <section className="image-section">
-        <img src="/telainicio.jpeg" alt="Login Background" className="background-image" />
-        <div className="overlay">
-          <div className="welcome-text">
-            <h1>Bem-vindo ao Unifeso Authenticator</h1>
-            <p>Sua plataforma de gerenciamento de Senhas e Tokens</p>
-          </div>
-        </div>
-      </section>
-      <section className="form-section">
-        <form id="loginForm">
-          <h2>LOGIN</h2>
-          <p>Por favor, insira suas credenciais para acessar sua conta.</p>
+	const validateFaceId = async () => {
+		if (!faceImg) {
+			return toast.error("Por favor, insira uma imagem")
+		}
 
-          <div className="form-group">
-            <label >Email</label>
-            <input
-              onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
-              type="email" id="email" name="email" placeholder="Email" required />
-          </div>
+		if (!credentials.email) {
+			return toast.error("Por favor, insira um email")
+		}
 
-          <div className="form-group">
-            <label>Senha</label>
-            <input
-              onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-              type="password" id="password" name="password" placeholder="Senha" required />
-            <span className="error-message" id="passwordError"></span>
-          </div>
+		const res = await faceId(credentials.email, faceImg)
+		console.log({ res })
+		if (res) {
+			return navigate("/")
+		}
+	}
 
-          <button 
-            type="button"
-            className="button"
-            onClick={validateForm}
-          >
-            Entrar
-          </button>
-          <Link
-            to={"/register"}
-            className="register-link"
-          >
-            Cadastrar
-          </Link>
-          <div className="additional-options">
-            <a href="#">Esqueci minha senha</a>
-          </div>
-          <p id="loginError" className="error-message"></p>
-        </form>
-      </section>
-    </main>
-  );
+	const validateForm = async () => {
+		if (await signIn(credentials.email, credentials.password)) {
+			return navigate("/validate-code")
+		}
+	}
+
+	return (
+		<main className="container">
+			<section className="image-section">
+				<img src="/telainicio.jpeg" alt="Login Background" className="background-image" />
+				<div className="overlay">
+					<div className="welcome-text">
+						<h1>Bem-vindo ao Unifeso Authenticator</h1>
+						<p>Sua plataforma de gerenciamento de Senhas e Tokens</p>
+					</div>
+				</div>
+			</section>
+
+			<section className="form-section">
+				<div
+					style={{
+						display: "flex",
+						gap: "2rem",
+						margin: "0 0 3rem 0",
+					}}
+				>
+					<p
+						style={{
+							color: screen === "login" ? "#000" : "#ccc",
+							cursor: "pointer",
+						}}
+						onClick={() => setScreen("login")}
+					>
+						Login
+					</p>
+					<p
+						style={{
+							color: screen === "faceid" ? "#000" : "#ccc",
+							cursor: "pointer",
+						}}
+						onClick={() => setScreen("faceid")}
+					>
+						FaceID
+					</p>
+				</div>
+				{screen === "login" ? (
+					<form id="loginForm">
+						<h2>LOGIN</h2>
+						<p>Por favor, insira suas credenciais para acessar sua conta.</p>
+
+						<div className="form-group">
+							<label>Email</label>
+							<input
+								onChange={(e) =>
+									setCredentials({ ...credentials, email: e.target.value })
+								}
+								type="email"
+								id="email"
+								name="email"
+								placeholder="Email"
+								required
+							/>
+						</div>
+
+						<div className="form-group">
+							<label>Senha</label>
+							<input
+								onChange={(e) =>
+									setCredentials({ ...credentials, password: e.target.value })
+								}
+								type="password"
+								id="password"
+								name="password"
+								placeholder="Senha"
+								required
+							/>
+							<span className="error-message" id="passwordError"></span>
+						</div>
+
+						<button type="button" className="button" onClick={validateForm}>
+							Entrar
+						</button>
+						<Link to={"/register"} className="register-link">
+							Cadastrar
+						</Link>
+						<div className="additional-options">
+							<a href="#">Esqueci minha senha</a>
+						</div>
+						<p id="loginError" className="error-message"></p>
+					</form>
+				) : (
+					<form id="loginForm">
+						<h2>Face ID</h2>
+						<p>Use uma imagem do seu rosto para dizer que é você mesmo</p>
+
+						<div className="form-group">
+							<label>Email</label>
+							<input
+								onChange={(e) =>
+									setCredentials({ ...credentials, email: e.target.value })
+								}
+								type="email"
+								id="email"
+								name="email"
+								placeholder="Email"
+								required
+							/>
+						</div>
+
+						<div className="form-group">
+							<label>Seu rosto</label>
+							<input
+								onChange={(e) =>
+									setFaceImg(e.target.files ? e.target.files[0] : null)
+								}
+								type="file"
+								id="password"
+								name="password"
+								placeholder="Senha"
+								required
+							/>
+							<span className="error-message" id="passwordError"></span>
+						</div>
+
+						<button
+							type="button"
+							className="button"
+							onClick={validateFaceId}
+							style={{
+								opacity: !(auth.status === "loading") ? 1 : 0.5,
+								cursor: !(auth.status === "loading") ? "pointer" : "not-allowed",
+							}}
+						>
+							Entrar
+						</button>
+						<Link to={"/register"} className="register-link">
+							Cadastrar
+						</Link>
+						<div className="additional-options">
+							<a href="#">Esqueci minha senha</a>
+						</div>
+						<p id="loginError" className="error-message"></p>
+					</form>
+				)}
+			</section>
+		</main>
+	)
 }
--- src/store/auth/auth.service.ts
+++ src/store/auth/auth.service.ts
@@ -1,54 +1,98 @@
-import { useAtom } from "jotai";
-import { authAtom } from "./auth.module";
-import { api } from "../../api";
-import { toast } from "react-toastify";
+import { useAtom } from "jotai"
+import { toast } from "react-toastify"
+import { api } from "../../api"
+import { authAtom } from "./auth.module"
 
 export const AuthService = () => {
-  const [auth, setAuth] = useAtom(authAtom);
+	const [auth, setAuth] = useAtom(authAtom)
 
+	console.log({ auth })
 
-  const signIn = async (email: string, password: string) => {
-    try {
-      setAuth({
-        ...auth,
-        status: "loading",
-      });
+	const signIn = async (email: string, password: string) => {
+		try {
+			setAuth({
+				...auth,
+				status: "loading",
+			})
 
-     await api.post("/auth/login", {
-        email,
-        password,
-     })
-     return true
-    } catch (e) {
-      toast.error("Erro ao fazer login");
-    }
-  }
+			await api.post("/auth/login", {
+				email,
+				password,
+			})
+			return true
+		} catch (e) {
+			toast.error("Erro ao fazer login")
+		}
+	}
 
-  const validadeCode = async (code: string) => {
-    try {
-      setAuth({
-        ...auth,
-        status: "loading",
-      });
+	const faceId = async (email: string, img: File) => {
+		const formData = new FormData()
+		formData.append("file", img)
+		try {
+			setAuth({
+				...auth,
+				status: "loading",
+			})
 
-    const {data} =  await api.post("/auth/validate", {
-        code,
-      })
-      
-      return data.token
-    } catch (e) {
-      toast.error("Erro ao validar código");
-      setAuth({
-        ...auth,
-        status: "error",
-      });
-    } 
-  }
+			const user = await api.get(`/user/email/${email}`)
+			const recog = await api.post(`/faceid/recog/${user.data.id}`, formData, {
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			})
+			if (recog.data.statusCode === 404) {
+				toast.error("Usuário não encontrado")
+			}
+			console.log({ recog }, img, formData.getAll("file"), user.data)
 
-  return {
-    auth,
-    signIn,
-    validadeCode,
-    setAuth
-  }
+			if (recog.data.statusCode === 500) {
+				toast.error("Erro ao fazer reconhecimento facial")
+				return false
+			}
+
+			if (recog.data.statusCode === 200) {
+				setAuth({
+					...auth,
+					status: "success",
+					data: {
+						authenticated: true,
+						token: recog.data.token,
+					},
+				})
+				return true
+			}
+			return false
+		} catch (e) {
+			toast.error("Erro ao fazer login")
+		}
+	}
+
+	const validadeCode = async (code: string) => {
+		try {
+			setAuth({
+				...auth,
+				status: "loading",
+			})
+
+			const { data } = await api.post("/auth/validate", {
+				code,
+			})
+
+			return data.token
+		} catch (e) {
+			toast.error("Erro ao validar código")
+			setAuth({
+				...auth,
+				status: "error",
+			})
+		}
+	}
+
+	return {
+		auth,
+		signIn,
+		validadeCode,
+		setAuth,
+		faceId,
+	}
 }
